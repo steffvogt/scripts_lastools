b6482fcc9446bc5060d3b6cf047e925477f484d6
Steffen Vogt <s.vogt@svgeosolutions.de>
2020-07-22 18:54:30 +0200

    added batch file that converts ground classified las file to xyz for Civil 3D
    Signed-off-by: Steffen Vogt <s.vogt@svgeosolutions.de>

---
+++ las_to_xyz_only_ground_comma_separated.bat
@@ -0,0 +1,42 @@
+::
+:: Batch script to transform ground points (classified as 2) from las file to a comma separated ASCII xyz file (e.g. for Civil3D)
+::
+:: Input is LAS file
+:: Output is a comma separated ASCII XYZ file with the same points
+::
+:: Parameters:
+::
+:: none
+::
+:: USAGE: las_to_xyz_comma_separated.bat mypointcloud.las
+::
+::
+:: Processing steps:
+:: las2text transform las to ASCII xyz
+::
+:: Steffen Vogt, 2020-07-22
+
+echo off
+
+::
+:: specify parameters
+::
+
+:: get the input las file name for ouptut file name generation
+set INFILE=%1
+set OUTFILE=%INFILE:~0,-4%_ground_points.txt
+
+:: allows you to run the script from other folders
+::set PATH=%PATH%;C:\Programme\LASTools\bin;
+set PATH=%PATH%;C:\LASTools\bin;
+
+
+:: Write ASCII xyz file
+las2txt -i %INFILE% ^
+        -o %OUTFILE% ^
+        -keep_class 2 ^
+        -parse xyz ^
+        -sep comma ^
+        -cpu64
+
+echo finished creating %OUTFILE%
